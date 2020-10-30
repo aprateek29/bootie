226253b608b3a73f032c0623168526e77331760c
Prateek <aprateek29@gmail.com>
2020-10-30 16:34:12 +0530

    added signup_view & html-form

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .admin import UserCreationForm, UserChangeForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
