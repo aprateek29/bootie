ecb14dea28fa7f0e4fbb4589a740aea536e8c335
Prateek <aprateek29@gmail.com>
2020-10-30 19:43:28 +0530

    added grid layout

--- src/pages/views.py
+++ src/pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'pages/index.html', {})
+    return render(request, 'pages/index.html', {'product_list':[1,2,3,4]})
