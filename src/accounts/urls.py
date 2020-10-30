5d62df4467fca4ce8983046d203003302c325230
Prateek <aprateek29@gmail.com>
2020-10-30 17:41:07 +0530

    added login view & html-form

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
+    path('login/', views.login_view, name='login'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render, redirect
 from .admin import UserCreationForm, UserChangeForm
 from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+
 
 def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            messages.info(request, f"New account created for {username}")
             form.save()
+            return redirect('/')
+        else:
+            messages.error(request, "Some error occurred")
     else:
         form = UserCreationForm()
     return render(request, 'accounts/signup.html', {'form':form})
@@ -14,3 +21,21 @@ def signup_view(request):
 def logout_view(request):
     logout(request)
     return redirect('/')
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect('/')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, "accounts/login.html", {"form":form})
