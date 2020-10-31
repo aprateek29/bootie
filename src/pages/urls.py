9a3a7b2e2485f2bd0dc99a45db96a943713ac5cd
Prateek <aprateek29@gmail.com>
2020-10-31 20:47:20 +0530

    added django-active-link

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
     path('about/', views.about_view, name='about'),
     path('blog/', views.blog_view, name='blog'),
     path('contact/', views.contact_view, name='contact'),
     path('single/', views.single_view, name='single'),
+    path('', views.index_view, name='index'),
 ]
