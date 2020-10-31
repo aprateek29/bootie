af94596a75ffb7f71cf332e3fc40cd35bdf47ab6
Prateek <aprateek29@gmail.com>
2020-10-31 19:22:35 +0530

    added new home, blog and about page

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='index')
+    path('', views.index_view, name='index'),
+    path('about/', views.about_view, name='about'),
+    path('blog/', views.blog_view, name='blog'),
 ]
--- src/pages/views.py
+++ src/pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'pages/index.html', {'product_list':[1,2,3,4]})
+
+def about_view(request):
+    return render(request, 'pages/about.html', {})
+
+def blog_view(request):
+    return render(request, 'pages/blog.html', {})
